017a1e6cb47ad49917fa528bf9c2a8684ddc0b32
EC2 Default User <ec2-user@ip-172-31-20-115.us-east-2.compute.internal>
2018-04-20 15:49:52 +0000

    Complete advanced setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'application#hello'
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -6,10 +10,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   
   get 'static_pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
-
-  root 'static_pages#home'
 
 end
